b839d409bbba4e2a83e5c833cd5a82c77e3c2f37
JohnPower <stkautoru@mail.ru>
2021-04-08 12:04:11 +0700

    init commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "Hello world!")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
